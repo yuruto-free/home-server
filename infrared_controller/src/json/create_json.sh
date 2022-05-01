513a21e6684fefc48eda235186a999a9ee372057
yuruto-free <mailorder2receiver@gmail.com>
2022-05-01 22:56:18 +0900

    エアコンと扇風機をサンプルとして追加。 上記の変更に合わせ、flows.jsonを更新。

--- infrared_controller/src/json/create_json.sh
+++ infrared_controller/src/json/create_json.sh
@@ -5,13 +5,27 @@ function mapping_light(){
     # ========================================
     # 操作ボタン番号（device_number）と
     # 制御名（control_name）の対応を記載
-    # 以下の例:
-    #   0番に蛍光灯をつけるときのボタンを登録
-    #   1番に蛍光灯を消すときのボタンを登録
     # ========================================
     # device_number control_name
-    echo 0 on     # light on
-    echo 1 off    # light off
+    echo 0 on          # light on
+    echo 1 off         # light off
+    echo 2 night_light # night light
+    echo 3 up          # brightness up
+    echo 4 down        # brightness down
+    echo 5 all_light   # all light
+}
+
+function mapping_air_conditioner(){
+    echo 0 cooler_on
+    echo 1 heater_on
+    echo 2 power_off
+    echo 3 dry_on
+}
+
+function mapping_electric_fan(){
+    echo 2 power_off
+    echo 0 power_on
+    echo 1 fan_up
 }
 
 # ============
@@ -23,6 +37,18 @@ exe_flag=0
 case "$1" in
     light )
         exe_func=mapping_light
+        exe_flag=1
+        shift
+        ;;
+
+    air )
+        exe_func=mapping_air_conditioner
+        exe_flag=1
+        shift
+        ;;
+
+    fan )
+        exe_func=mapping_electric_fan
         exe_flag=1
         shift
         ;;
